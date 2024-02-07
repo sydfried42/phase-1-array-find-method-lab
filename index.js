ff673bf320394ed6be8235e7743f371810bc4aca
sydneyfriedman <friedman.sydneyl@gmail.com>
2024-02-07 16:20:57 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+// const record = [
+//     { year: "2015", result: "W"},
+//     { year: "2014", result: "N/A"},
+//     { year: "2013", result: "L"},
+//     //...
+//   ]
+
+function superbowlWin(record) {
+    for (let idv of record) {
+    if (idv.result === "W") {
+    return idv.year };
+}};
+
+console.log(record.find(superbowlWin));
